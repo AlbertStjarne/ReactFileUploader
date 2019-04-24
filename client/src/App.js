09ea8f0e65a9c44fdf63bdfa6d67a7e849eff6ad
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-24 18:39:17 +0200

    adds basic html to App file

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 
 const App = () => (
-    <div className="App">
-      app
+    <div className='container mt-4'>
+      <h4 className="display-4 text-center mb-4">
+        <i className="fab fa-react">React File Upload</i>
+      </h4>
     </div>
   );
 
